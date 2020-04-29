9f32c6d20457bae8be6ba73539c342584ae6d1ad
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-04-29 14:49:32 -0300

    update POST to imobiliarias

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -8,6 +8,20 @@ app.use(express.json());
 const PORT = 3333;
 
 const imobiliarias = [];
+
+// ---- Middleware
+// Valida se ID da imobiliaria é valido
+function validateImobiliariaId(request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "ID imobiliaria invalido" })
+  }
+  return next();
+}
+
+app.use('/imobiliarias/:id', validateImobiliariaId);
+// ----
 
 app.get('/imobiliarias', (request, response) => {
   return response.json(imobiliarias);
@@ -26,22 +40,22 @@ app.post('/imobiliarias', (request, response) => {
 
   imobiliarias.push(imobiliaria);
 
-  return response.json(imobiliarias);
+  return response.json(imobiliaria);
 })
 
 app.put('/imobiliarias/:id', (request, response) => {
   const { id } = request.params;
   const { nome, contato, email, telefone } = request.body;
 
-  const imobiliariafind = imobiliarias.find(imobiliaria => 
+  const imobiliariafind = imobiliarias.findIndex(imobiliaria => 
     imobiliaria.id === id
-  );
-
-  if (!imobiliariafind) {
+  ); 
+ // Valida se ID da imobiliaria existe/localizar no array
+  if (imobiliariafind < 0) {
     return response.status(400).json({
       error: 'ID da imobiliaria não localizado'
     });
-  }
+  } 
 
   const imobiliaria = {
     id,
