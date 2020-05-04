7650a561ad32c672f2ec2d983d62a2862396be3c
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-05-04 12:18:45 -0300

    Add POST imobiliarias at routes

--- src/routes/imobiliariasRouter.js
+++ src/routes/imobiliariasRouter.js
@@ -1,11 +1,40 @@
 const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
 
 const imobiliariasRoutee = express.Router();
 
+const imobiliarias = [];
+/*
+// ---- Middleware
+// Valida se ID da imobiliaria é valido
+function validateImobiliariaId(request, response, next) {
+  const { id } = request.params
 
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "ID imobiliaria invalido" })
+  }
+  return next();
+}
 
+express.use('/imobiliarias/:id', validateImobiliariaId);
+*/
 imobiliariasRoutee.post('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+  const { nome, contato, email, telefone } = request.body;
+
+  //vefificar se imobiliaria existe/foi criada anteriormente.
+  
+
+  const imobiliaria = {
+    id: uuid(),
+    nome,
+    contato,
+    email,
+    telefone
+  };
+
+  imobiliarias.push(imobiliaria);
+
+  return response.json(imobiliaria);
 });
 
 module.exports = imobiliariasRoutee;
